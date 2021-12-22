7adb20b5cb4b341862a88ba3e06f376c8e65eeea
Cornerstone <fortunecornerstone@gmail.com>
2021-12-22 01:27:34 +0100

    Fixed react declaration. Adjeusted styling Added links to icons.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./App.css";
 import Generator from "./components/QRCode";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App" >
+    <div className="App">
       <Generator />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,23 @@ import "../styles/footer.scss";
 const Footer = () => {
   return (
     <footer>
-      <div className="icons" >
-        <i> Ephraim Cornerstone &copy;2020</i> 
+      <div className="icons">
+        <i> Ephraim Cornerstone &copy;2020</i>|
+        <a href="https://www.linkedin.com/in/cornerstone-ephraim/">
+          <i className="fab fa-github-alt"></i>
+        </a>
         |
-        <i className="fab fa-github-alt"></i>
+        <a href="https://www.facebook.com/cornerstone-ephraim/">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        |{" "}
+        <a href="https://www.instagram.com/cornerstone.01/">
+          <i className="fab fa-instagram"></i>
+        </a>
         |
-        <i className="fab fa-facebook-f"></i>
-        |
-        <i className="fab fa-instagram"></i>
-        |
-        <i className="fab fa-linkedin-in"></i>
+        <a href="https://github.com/Cornerstone-04/">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
       </div>
     </footer>
   );
